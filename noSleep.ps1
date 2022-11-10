ffee5f5285c05ad724bd27091504491017f8f2cf
abhilashreddysh <116741403+abhilashreddysh@users.noreply.github.com>
2022-11-10 19:13:48 +0530

    Update noSleep.ps1 [v2.3]
    1. Script will now end after the logout time
    2. Ask for Hour and Minutes to end the script

--- noSleep.ps1
+++ noSleep.ps1
@@ -1,12 +1,10 @@
 #
-# Title : NoSleep
-# Goal : Keep your computer awake by programmatically pressing the ScrollLock key every X seconds
+# -- NoSleep --
+# Goal : Keep your computer awake by programmatically pressing the Scroll Lock key every X seconds
 # Owner : Abhi
-# Version : 2.0    
-# Changelog : 
-# v1.0  :   Toggle Scroll Lock for every x Seconds
-# v2.0  :   Script will now end after the end time
-#           Proper messages of what is happening in the script
+# Version : 2.3    
+# Changelog : Script will now end after the logout time
+#             Ask for Hour and Minutes to end the script
 #
 
 param($sleep = 120) # seconds
@@ -25,43 +23,52 @@ try {
    Write-Host "Couldn't start the stopwatch."
 }
 
-Write-Host "Executing ScrollLock-toggle NoSleep."
-Write-Host "Start time:" $(Get-Date -Format "dddd MM/dd HH:mm (K)") -fore red
+Write-Host "Executing ScrollLock-toggle NoSleep  [Ver : 2.1]"
+Write-Host "Start time:" $(Get-Date -Format "dddd MM/dd HH:mm (K)") -fore magenta
 
 $index = 0
-Write-Host "The default time is 5:00 PM." -fore green
-$Hour = (Read-Host -Prompt 'Input your end time in 24H format greater than 0') -as [int]
+$Min = 00
+Write-Host "`nInput your end time in 24H format greater than 0" -fore red
+Write-Host "The default time is 6:00 PM.`n" -fore green
+$Hour = (Read-Host -Prompt 'Hour') -as [int]
 if($Hour -isnot [int] -or ($Hour -eq 0)){
-    Write-Host "`nYour input has to be a number greater than 0." -fore magenta
-    Write-Host "`nSetting the default time as 5:00 PM." -fore green
-    $Hour = 17
+    Write-Host "`nYour input has to be a number greater than 0." -fore red
+    Write-Host "`nSetting the default time as 6:00 PM." -fore green
+    $Hour = 18
 }else{
-    Write-Host "Your end time is "$Hour":00" -fore green
+    $Min = (Read-Host -Prompt 'Minutes') -as [int]
+    if($Min -isnot [int] -or ($Min -eq 0)){
+        Write-Host "`nYour input has to be a number greater than 0." -fore red
+        Write-Host "`nSetting the default time as "$Hour":00 PM." -fore green
+        $Min = 00
+    }else{
+        Write-Host "Your end time is "$Hour":"$Min"." -fore green
+    }
 }
 
 
 $DateTimeNow = Get-Date
 $DateTimeLogout = Get-Date -Hour $Hour -Minute 00      # by default Script will not run after 5:00 PM
 if($DateTimeNow.TimeOfDay -lt $DateTimeLogout.TimeOfDay){
-    Write-Host "Script started!!! Enjoy your time <3`n" -fore green
-    Write-Host "Use Ctrl+c to exit the script."
+    Write-Host "Script started!!! Enjoy your time <3" -fore green
+    Write-Host "`nUse Ctrl+c to exit the script."
     while ( $true )
     {
         $DateTimeNow = Get-Date
         if($DateTimeNow.TimeOfDay -lt $DateTimeLogout.TimeOfDay){
-            $WShell.sendkeys("{SCROLLLOCK}")
-            Start-Sleep -Seconds 1
-            $WShell.sendkeys("{SCROLLLOCK}")
+            $WShell.sendkeys("{NUMLOCK}")
+            Start-Sleep -Milliseconds 10
+            $WShell.sendkeys("{NUMLOCK}")
             Start-Sleep -Seconds $sleep
             # Announce runtime on an interval
             if ( $stopwatch.IsRunning -and (++$index % $announcementInterval) -eq 0 ){
-                $Title = "I am AWAKE!!, Elapsed time: " + $stopwatch.Elapsed.ToString('dd\.hh\:mm\:ss')
+                $Title = "I am ONLINE!!, Elapsed time: " + $stopwatch.Elapsed.ToString('dd\.hh\:mm\:ss')
                 $host.UI.RawUI.WindowTitle = $Title
         }
         }else{
+            Write-Host "Congratulations for completing the day." -fore green
             Write-Host "Elapsed time: " + $stopwatch.Elapsed.ToString('dd\.hh\:mm\:ss')
-            Write-Host "Script Complete!!!!"
-            $host.UI.RawUI.WindowTitle = "Script Complete!!! Script is not currently active."
+            $host.UI.RawUI.WindowTitle = "Day Complete!!! Script is not running now."
             Write-Host "`nTo close, press any key..."
             $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
             exit
