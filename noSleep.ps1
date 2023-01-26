d62d9d1de72d3f865e5c3c141bc0295a57e3b5b2
Abhilash Reddy S H <116741403+abhilashreddysh@users.noreply.github.com>
2023-01-26 12:49:10 +0530

    Update noSleep.ps1 [2.4]
    Track mouse movements and skip scroll lock toggle.

--- noSleep.ps1
+++ noSleep.ps1
@@ -5,15 +5,16 @@
 # Version : 2.3    
 # Changelog : Script will now end after the logout time
 #             Ask for Hour and Minutes to end the script
-#
+#             Skip Scroll Lock toggle if there is any mouse movement between the time interval
+
 
 param($sleep = 120) # seconds
-$announcementInterval = 1 # loops
+$announcementInterval = 2 # loops
 # Clear the current screen
 Clear-Host
 
 $WShell = New-Object -com "Wscript.Shell"
-$date = Get-Date -Format "dddd MM/dd HH:mm (K)"
+# $date = Get-Date -Format "dddd MM/dd HH:mm (K)"
 
 $stopwatch
 # Some environments don't support invocation of this method.
@@ -23,7 +24,7 @@ try {
    Write-Host "Couldn't start the stopwatch."
 }
 
-Write-Host "Executing ScrollLock-toggle NoSleep  [Ver : 2.1]"
+Write-Host "Executing ScrollLock-toggle NoSleep  [Ver : 2.4]"
 Write-Host "Start time:" $(Get-Date -Format "dddd MM/dd HH:mm (K)") -fore magenta
 
 $index = 0
@@ -46,38 +47,46 @@ if($Hour -isnot [int] -or ($Hour -eq 0)){
     }
 }
 
-
+Add-Type -AssemblyName System.Windows.Forms
+$pos2 = [System.Windows.Forms.Cursor]::Position
 $DateTimeNow = Get-Date
-$DateTimeLogout = Get-Date -Hour $Hour -Minute 00      # by default Script will not run after 5:00 PM
+$DateTimeLogout = Get-Date -Hour $Hour -Minute $Min      # by default Script will not run after 5:00 PM
 if($DateTimeNow.TimeOfDay -lt $DateTimeLogout.TimeOfDay){
     Write-Host "Script started!!! Enjoy your time <3" -fore green
     Write-Host "`nUse Ctrl+c to exit the script."
+    
     while ( $true )
     {
+        $pos1 = [System.Windows.Forms.Cursor]::Position
         $DateTimeNow = Get-Date
-        if($DateTimeNow.TimeOfDay -lt $DateTimeLogout.TimeOfDay){
-            $WShell.sendkeys("{NUMLOCK}")
-            Start-Sleep -Milliseconds 10
-            $WShell.sendkeys("{NUMLOCK}")
+        if($DateTimeNow.TimeOfDay -lt $DateTimeLogout.TimeOfDay){  
+            if($pos1.X -eq $pos2.X -and $pos1.Y -eq $pos2.Y) {
+                $WShell.sendkeys("{SCROLLLOCK}")
+                Start-Sleep -Milliseconds 10
+                $WShell.sendkeys("{SCROLLLOCK}")
+            }
+            else {
+                # pass
+            }
+            $pos2 = [System.Windows.Forms.Cursor]::Position
             Start-Sleep -Seconds $sleep
+            
             # Announce runtime on an interval
             if ( $stopwatch.IsRunning -and (++$index % $announcementInterval) -eq 0 ){
                 $Title = "I am ONLINE!!, Elapsed time: " + $stopwatch.Elapsed.ToString('dd\.hh\:mm\:ss')
                 $host.UI.RawUI.WindowTitle = $Title
-        }
+            }
         }else{
             Write-Host "Congratulations for completing the day." -fore green
             Write-Host "Elapsed time: " + $stopwatch.Elapsed.ToString('dd\.hh\:mm\:ss')
             $host.UI.RawUI.WindowTitle = "Day Complete!!! Script is not running now."
-            Write-Host "`nTo close, press any key..."
-            $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
             exit
         }
     }
 }
 else{
         Write-Host "`n"$DateTimeNow
-        Write-Host "`nThe end time has passed. Script ended." -fore yellow
+        Write-Host "`nHey, the hour has come. Don't pretend you are working :(`n`nYou are not needed to use this script; you may leave right away! :)" -fore yellow
         Write-Host "`nTo close, press any key..."
         $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
         exit
