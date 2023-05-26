4ed3c23090a694bb926edcdead03d56756878a3a
Abhilash Reddy S H <116741403+abhilashreddysh@users.noreply.github.com>
2023-05-26 10:40:02 +0530

    Delete config.py
    remove sensitive data

--- LinuxPythonMail/config.py
+++
@@ -1,4 +0,0 @@
-gmailUser = 'abhilashreddy.notify@gmail.com'
-gmailPassword = 'fopqydwacupvokzs'
-recipient = 'tendec99@gmail.com'
-services_to_be_monitered = ['smbd','nginx','sshd','deluged','deluge-web']
