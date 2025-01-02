a3811ff5bf6ce9926fcc04f41be1b47fa45bde46
Abhilash Reddy <abhilashreddysh.99@gmail.com>
2025-01-02 11:29:27 +0530

    commit-02012025

--- WebConsole/js/main.js
+++ WebConsole/js/main.js
@@ -7,13 +7,3 @@ for (const tdstatus of stats) {
     tdstatus.id = "status-inactive";
   }
 }
-
-let stats = document.getElementsByClassName("status");
-
-for (const tdstatus of stats) {
-  if (tdstatus.outerText === "ACTIVE") {
-    tdstatus.id = "status-active";
-  } else {
-    tdstatus.id = "status-inactive";
-  }
-}
